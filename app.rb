7cb5d39d87dbeca9ed4f9a19c8cc3bc1557714d7
Joe Dye <jdye94@gmail.com>
2017-02-22 16:41:13 -0500

    added data

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/content_for'
+require 'yaml'
 
 get '/' do
+  general_data = YAML.load_file('data/general.yml')
+  paragraph_data = YAML.load_file('data/paragraphs.yml')
   erb :index, :locals => {:name => 'juck'}
-end
+end 
