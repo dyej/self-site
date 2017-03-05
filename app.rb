f5f1b394a9bacc871a6db792d9043ac67a259920
Joe Dye <jdye94@gmail.com>
2017-03-05 15:53:43 -0500

    looping over content from data files

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require 'sinatra/content_for'
 require 'yaml'
 
 get '/' do
-  general_data = YAML.load_file('data/general.yml')
-  paragraph_data = YAML.load_file('data/paragraphs.yml')
-  erb :index, :locals => {:name => 'juck'}
+  @general_data = YAML.load_file('data/general.yml')
+  @paragraph_data = YAML.load_file('data/paragraphs.yml')
+  erb :index
 end 
+
+#ERB is just ruby
+# <% @collection.each do |value| %>
+#   <%= value %>
+# <% end %>
