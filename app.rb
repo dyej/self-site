19fe33de94edda42691c3aa603d92c8c9b87af5e
Joe Dye <jdye94@gmail.com>
2017-04-09 21:42:37 -0400

    pdf progress

--- app.rb
+++ app.rb
@@ -4,20 +4,42 @@ require 'yaml'
 
 @@general_data = YAML.load_file('data/general.yml')
 @@paragraph_data = YAML.load_file('data/paragraphs.yml')
+@@num_uploads = 5
 
 configure do
-  set :environment, "development"
+ # set :environment, "production"
+end
+
+helpers do
+  def pdf_combine(params)
+    params.each do |param|
+      puts param
+      # if param[:name].include? 'file' #&& !param[:filename].nil?
+      #   puts param[:name]
+      # end
+    end
+  end
+
+  def pdf_add()
+    puts "add"
+  end
 end
 
 get '/' do 
   erb :index
 end
 
-get '/utils/upload' do
-  erb :upload
-end
-
 get '/utils/pdf' do
-  @num_uploads = 5
   erb :pdf
 end 
+
+post '/utils/pdf/combine' do
+  if params[:commit] == '+'
+    @@num_uploads += 1
+    puts params
+    pdf_add()
+    redirect to('/utils/pdf')
+  elsif params[:commit] == 'Submit'
+    @combined_pdf = pdf_combine(params)
+  end
+end
